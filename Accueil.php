1154fc021726d4c6ebfeffd368ba74e3931c1505
arkeo <trillejs@gmail.com>
2014-10-31 15:02:49 +0100

    Création du projet

---
+++ Accueil.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+    <title> Accueil </title>
+    <link rel="stylesheet" href="Accueil.css" />
+    <meta charset="utf-8" />
+    <title>ChasseoLab</title>
+    </head>
+ 
+    <body>
+    <?php include("header.php"); ?> 
+    </body>
+	
+</html>
---
+++ header.php
@@ -0,0 +1,7 @@
+<p> <h1>ChasseoLab</h1></p>
+<p><ul>
+    <li>Actualités</li>
+    <li>Présentation</li>
+    <li>Equipe</li>
+
+</ul></p>
